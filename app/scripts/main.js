f60ae888cf0294be142827402a724fc529541dd1
Shawn Mathew <shawn.mathew@agencychief.com>
2019-07-18 19:14:09 -0400

    seating updates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,56 @@
-console.log('\'Allo \'Allo!');
+( function() {
+  const guests = Array.from( document.querySelectorAll('.tableSeating__guest') );
+  const searchInput = document.querySelector('.tableSeating__searchInput');
+  const searchIcon = document.querySelector('.tableSeating__searchIcon');
+  const searchIconClose = document.querySelector('.tableSeating__searchIconClose');
 
-// Uncomment to enable Bootstrap tooltips
-// https://getbootstrap.com/docs/4.0/components/tooltips/#example-enable-tooltips-everywhere
-// $(function () { $('[data-toggle="tooltip"]').tooltip(); });
+  const displayAllGuests = () => {
+    guests.forEach( guest => {
+      if ( guest.classList.contains('hide') ) guest.classList.remove('hide');
+    } );
+  };
 
-// Uncomment to enable Bootstrap popovers
-// https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
-// $(function () { $('[data-toggle="popover"]').popover(); });
+  const toggleSearchIcons = value => {
+    if ( !value ) {
+      searchIcon.classList.remove('hide');
+      searchIconClose.classList.remove('show');
+      return;
+    }
+
+    if ( value === '' ) {
+      if ( searchIcon.classList.contains('hide') ) {
+        searchIcon.classList.remove('hide');
+        searchIconClose.classList.remove('show');
+      } 
+    } else {
+      searchIcon.classList.add('hide');
+      searchIconClose.classList.add('show');
+    }    
+  };
+
+  searchInput.addEventListener( 'input', function(){    
+    const value = this.value.toLowerCase().trim();
+
+    toggleSearchIcons( value );
+    
+    if ( value === '' ) {
+      displayAllGuests();  
+      return;
+    }
+    
+    guests.forEach( guest => {
+      const name = guest.querySelector('.name').textContent.toLowerCase();
+      if ( !name.includes( value ) ) {
+        guest.classList.add('hide');
+      } else {
+        guest.classList.remove('hide');
+      }
+    } );
+  } );
+
+  searchIconClose.addEventListener( 'click', () => {
+    searchInput.value = '';
+    toggleSearchIcons();
+    displayAllGuests();
+  } )
+} )();
